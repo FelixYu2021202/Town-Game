2fc80d3bab1d2fd55016b53153d18aa8f1ea77a7
FelixYu2021202 <1276273366@qq.com>
2023-06-18 20:49:30 +0800

    v0.6.2

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Game by cosf
+// v0.6.2
+// build 2023.6.6
+// unf
+// for more details please go to http://ac.hfoj.net/blog/35/6464e26c426c19a595da2748#1684333164277
+
+// Utilities
+
+#include "tg.h"
+
+// Game Board
+
+GameBoard gb;
+
+// Game Start
+int main()
+{
+    if (gb.game_start())
+    {
+        cout << "Game did not exit normally." << endl;
+        return -1;
+    }
+    return 0;
+}
