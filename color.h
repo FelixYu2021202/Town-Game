567177cd9af10bd78afc8ad910d8e0f9dd43ab48
FelixYu2021202 <1276273366@qq.com>
2023-06-29 19:01:23 +0800

    v0.7.0(fix)

--- color.h
+++ color.h
@@ -5,7 +5,7 @@
 #define USE_COLOR 0
 #define SET_GNU_COLOR_IF_AVAILABLE 1
 #define REVERSE_COLOR 1
-#define USE_NAMESPACE 0
+#define USE_NAMESPACE 1
 
 #include <iostream>
 #include <functional>
@@ -14,11 +14,10 @@
 #endif // (defined(__WINDOWS_) || defined(_WIN32))
 using namespace std;
 
-class Color
+class ColorWrapper
 {
 private:
-    bool gnu = SET_GNU_COLOR_IF_AVAILABLE;
-    void check_gnu_color()
+    bool gnu()
     {
 #if (defined(__WINDOWS_) || defined(_WIN32))
         string vname;
@@ -29,77 +28,84 @@ private:
         proc(&dwMajor, &dwMinor, &dwBuildNumber);
         if (dwMajor < 10)
         {
-            gnu = false;
+            return false;
         }
 #endif // (defined(__WINDOWS_) || defined(_WIN32))
+        return true;
     }
-
-public:
-    typedef void __cpf(ostream &os);
 
 private:
     template <int light, int fore, int back>
     class GnuWrapper
     {
     public:
-        static void sentry(ostream &os)
+        template <typename _CT, typename _T>
+        static basic_ostream<_CT, _T> &sentry(basic_ostream<_CT, _T> &os)
         {
-            os << string("\e[") + (light ? "1;" : "") + to_string(fore) + (back != 47 ? "m\e[" + to_string(back) : "") + "m";
+            os << string("\e[") + (light ? "1;" : "") + to_string(fore) + (back != 47 ? ";" + to_string(back) : "") + "m";
+            return os;
         }
     };
     template <int light, int fore, int back>
     class WdsWrapper
     {
     public:
-        static void sentry(ostream &os)
+        template <typename _CT, typename _T>
+        static basic_ostream<_CT, _T> &sentry(basic_ostream<_CT, _T> &os)
         {
 #if (defined(__WINDOWS_) || defined(_WIN32))
             SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (fore - 30) | (light << 3) | ((back - 40) << 4));
-#else  // (defined(__WINDOWS_) || defined(_WIN32))
+#elif !SET_GNU_COLOR_IF_AVAILABLE // (defined(__WINDOWS_) || defined(_WIN32))
             assert(("Unable to use gnu color renderer or windows color renderer!", false));
-#endif // (defined(__WINDOWS_) || defined(_WIN32))
+#endif                            // (defined(__WINDOWS_) || defined(_WIN32))
+            return os;
         }
     };
     template <int light, int fore, int back>
     class NullWrapper
     {
     public:
-        static void sentry(ostream &os)
+        template <typename _CT, typename _T>
+        static basic_ostream<_CT, _T> &sentry(basic_ostream<_CT, _T> &os)
         {
-            return;
+            return os;
         }
     };
-    template <int fore, int back, int light>
-    inline Color::__cpf &define_wrapper()
+    template <int fore, int back, int light, typename _CT, typename _T>
+    inline decltype(endl<_CT, _T>) &define_wrapper()
     {
-        if (gnu)
+        if (SET_GNU_COLOR_IF_AVAILABLE && gnu())
         {
-            return GnuWrapper<light, fore, back>::sentry;
+            return GnuWrapper<light, fore, back>::template sentry<_CT, _T>;
         }
         else
         {
-            return WdsWrapper<light, fore, back>::sentry;
+            return WdsWrapper<light, fore, back>::template sentry<_CT, _T>;
         }
     }
 
 public:
-    template <int fore, int back, int light>
-    inline __cpf &wrapper()
+    template <int fore, int back, int light, typename _CT, typename _T>
+    inline decltype(endl<_CT, _T>) &wrapper()
     {
-        return define_wrapper<fore, back, light>();
+        return define_wrapper<fore, back, light, _CT, _T>();
     }
-    inline __cpf &null_wrapper()
+    template <typename _CT, typename _T>
+    inline decltype(endl<_CT, _T>) &null_wrapper()
     {
-        return NullWrapper<0, 0, 0>::sentry;
+        return NullWrapper<0, 0, 0>::sentry<_CT, _T>;
     }
-    Color()
-    {
-        check_gnu_color();
-    }
-} color_wrapper;
+};
 
-#define DC(name, fore, back, light) inline function<void(basic_ostream<char> &)> name = color_wrapper.wrapper<fore, back, light>();
-#define DN(name) inline function<void(basic_ostream<char> &)> name = color_wrapper.null_wrapper();
+static ColorWrapper color_wrapper;
+#define DEFINE_COLOR(name, fore, back, light)                           \
+    template <typename _CT, typename _T>                                \
+    inline basic_ostream<_CT, _T> &name(basic_ostream<_CT, _T> &os)     \
+    {                                                                   \
+        return color_wrapper.wrapper<fore, back, light, _CT, _T>()(os); \
+    }
+#define STD_COLOR(fore, back, light) color_wrapper.wrapper<fore, back, light, char>()
+#define STD_COLOR_RESET color_wrapper.null_wrapper<char>()
 
 #if USE_NAMESPACE
 namespace color
@@ -109,74 +115,65 @@ namespace color
 #if USE_COLOR
 
 #if REVERSE_COLOR
-
-    DC(black, 30, 47, 0);
-    DC(red, 31, 47, 0);
-    DC(green, 32, 47, 0);
-    DC(yellow, 33, 47, 0);
-    DC(blue, 34, 47, 0);
-    DC(magenta, 35, 47, 0);
-    DC(cyan, 36, 47, 0);
-    DC(white, 37, 47, 0);
-    DC(light_black, 30, 47, 1);
-    DC(light_red, 31, 47, 1);
-    DC(light_green, 32, 47, 1);
-    DC(light_yellow, 33, 47, 1);
-    DC(light_blue, 34, 47, 1);
-    DC(light_magenta, 35, 47, 1);
-    DC(light_cyan, 36, 47, 1);
-    DC(light_white, 37, 47, 1);
-
-#else // REVERSE_COLOR
-
-    DC(black, 30, 40, 0);
-    DC(red, 31, 40, 0);
-    DC(green, 32, 40, 0);
-    DC(yellow, 33, 40, 0);
-    DC(blue, 34, 40, 0);
-    DC(magenta, 35, 40, 0);
-    DC(cyan, 36, 40, 0);
-    DC(white, 37, 40, 0);
-    DC(light_black, 30, 40, 1);
-    DC(light_red, 31, 40, 1);
-    DC(light_green, 32, 40, 1);
-    DC(light_yellow, 33, 40, 1);
-    DC(light_blue, 34, 40, 1);
-    DC(light_magenta, 35, 40, 1);
-    DC(light_cyan, 36, 40, 1);
-    DC(light_white, 37, 40, 1);
-
+    DEFINE_COLOR(black, 30, 47, 0)
+    DEFINE_COLOR(red, 31, 47, 0)
+    DEFINE_COLOR(green, 32, 47, 0)
+    DEFINE_COLOR(yellow, 33, 47, 0)
+    DEFINE_COLOR(blue, 34, 47, 0)
+    DEFINE_COLOR(magenta, 35, 47, 0)
+    DEFINE_COLOR(cyan, 36, 47, 0)
+    DEFINE_COLOR(white, 37, 47, 0)
+    DEFINE_COLOR(light_black, 30, 47, 1)
+    DEFINE_COLOR(light_red, 31, 47, 1)
+    DEFINE_COLOR(light_green, 32, 47, 1)
+    DEFINE_COLOR(light_yellow, 33, 47, 1)
+    DEFINE_COLOR(light_blue, 34, 47, 1)
+    DEFINE_COLOR(light_magenta, 35, 47, 1)
+    DEFINE_COLOR(light_cyan, 36, 47, 1)
+    DEFINE_COLOR(light_white, 37, 47, 1)
+#else  // REVERSE_COLOR
+    DEFINE_COLOR(black, 30, 40, 0)
+    DEFINE_COLOR(red, 31, 40, 0)
+    DEFINE_COLOR(green, 32, 40, 0)
+    DEFINE_COLOR(yellow, 33, 40, 0)
+    DEFINE_COLOR(blue, 34, 40, 0)
+    DEFINE_COLOR(magenta, 35, 40, 0)
+    DEFINE_COLOR(cyan, 36, 40, 0)
+    DEFINE_COLOR(white, 37, 40, 0)
+    DEFINE_COLOR(light_black, 30, 40, 1)
+    DEFINE_COLOR(light_red, 31, 40, 1)
+    DEFINE_COLOR(light_green, 32, 40, 1)
+    DEFINE_COLOR(light_yellow, 33, 40, 1)
+    DEFINE_COLOR(light_blue, 34, 40, 1)
+    DEFINE_COLOR(light_magenta, 35, 40, 1)
+    DEFINE_COLOR(light_cyan, 36, 40, 1)
+    DEFINE_COLOR(light_white, 37, 40, 1)
 #endif // REVERSE_COLOR
 
 #else // USE_COLOR
 
-DN(black);
-DN(red);
-DN(green);
-DN(yellow);
-DN(blue);
-DN(magenta);
-DN(cyan);
-DN(white);
-DN(light_black);
-DN(light_red);
-DN(light_green);
-DN(light_yellow);
-DN(light_blue);
-DN(light_magenta);
-DN(light_cyan);
-DN(light_white);
+DEFINE_COLOR(black, 0, 0, 0)
+DEFINE_COLOR(red, 0, 0, 0)
+DEFINE_COLOR(green, 0, 0, 0)
+DEFINE_COLOR(yellow, 0, 0, 0)
+DEFINE_COLOR(blue, 0, 0, 0)
+DEFINE_COLOR(magenta, 0, 0, 0)
+DEFINE_COLOR(cyan, 0, 0, 0)
+DEFINE_COLOR(white, 0, 0, 0)
+DEFINE_COLOR(light_black, 0, 0, 0)
+DEFINE_COLOR(light_red, 0, 0, 0)
+DEFINE_COLOR(light_green, 0, 0, 0)
+DEFINE_COLOR(light_yellow, 0, 0, 0)
+DEFINE_COLOR(light_blue, 0, 0, 0)
+DEFINE_COLOR(light_magenta, 0, 0, 0)
+DEFINE_COLOR(light_cyan, 0, 0, 0)
+DEFINE_COLOR(light_white, 0, 0, 0)
 
 #endif // USE_COLOR
 
 #if USE_NAMESPACE
 }
 #endif // USE_NAMESPACE
-
-ostream &operator<<(ostream &os, function<void(basic_ostream<char> &)> pf)
-{
-    return pf(os), os;
-}
 
 #undef SET_GNU_COLOR_IF_AVAILABLE
 #undef REVERSE_COLOR
